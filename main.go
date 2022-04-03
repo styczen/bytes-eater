e3db22c3930f8e1b9a067c0dc9442d6a243c7aab
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2022-04-03 17:59:09 +0200

    Handling EOF

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	fileName := flag.String("f", "", "file to read from")
-    length := flag.Uint("l", 16, "number of bytes displayed in one line")
+	length := flag.Uint("l", 16, "number of bytes displayed in one line")
 	flag.Parse()
 
 	// Check whether passed path is path to file
@@ -22,7 +23,7 @@ func main() {
 		log.Fatal("passed path is a directory but it should be a path to file")
 	}
 
-    // Open file and handle error
+	// Open file and handle error
 	f, err := os.Open(*fileName)
 	if err != nil {
 		log.Fatal(err)
@@ -30,33 +31,46 @@ func main() {
 	defer f.Close()
 
 	b := make([]byte, 1)
-    var i uint
+	var i uint
+	var bytesStr, charsStr string
 	for {
-		_, err := f.Read(b)
+		n, err := f.Read(b)
+		if n == 0 {
+			fmt.Println()
+			break
+		}
 		if err != nil {
 			log.Fatal(err)
 		}
 
-        // Convert byte value to HEX to pad with '0'
-	    hexStr := strconv.FormatUint(uint64(b[0]), 16)
+		// Convert byte value to HEX to pad with '0'
+		hexStr := strconv.FormatUint(uint64(b[0]), 16)
 		if len(hexStr) == 1 {
-			fmt.Printf("0%v ", hexStr)
+			bytesStr += fmt.Sprintf("0%v ", hexStr)
 		} else {
-			fmt.Printf("%v ", hexStr)
+			bytesStr += fmt.Sprintf("%v ", hexStr)
 		}
 
-        // Check whether byte is "viewable" in a nice way
-        if b[0] > 32 && b[0] < 127 {
-			fmt.Printf("%v | ", string(b[0]))
-        } else {
-			fmt.Printf(". | ")
-        }
+		nrSpaces := *length - (i + 1)
+		bytesWithSpaces := bytesStr + strings.Repeat("   ", int(nrSpaces))
+		bytesWithSpaces += "| "
 
-        // Add new line if needed
-        i += 1
-        if i == *length {
-            fmt.Println()
-            i = 0
-        }
+		// Check whether byte is "viewable" in a nice way
+		if b[0] > 32 && b[0] < 127 {
+			charsStr += fmt.Sprintf("%v", string(b[0]))
+		} else {
+			charsStr += "."
+		}
+
+		// Print prepared line
+		fmt.Printf("\r%v", bytesWithSpaces+charsStr)
+
+		// Add new line if needed
+		i += 1
+		if i == *length {
+			fmt.Println()
+			i = 0
+			bytesStr, charsStr = "", ""
+		}
 	}
 }
